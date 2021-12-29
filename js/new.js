ca039c904962034ae460db13f03baf2cb4a832be
Jerry William <wilguepardo@gmail.com>
2021-12-29 17:06:40 -0300

    Projeto App Cálculo de IMC - 6ª modificação (Alteração de arquivo new.js e personalização de mensagens de acordo com o cálculo de cada IMC)

--- js/new.js
+++ js/new.js
@@ -1,21 +1,37 @@
-var form = document.getElementById('form');
-var nome = document.getElementById('nome');
-var res = document.getElementById('resImc');
+let form = document.getElementById('form');
+let name = document.getElementById('nome');
+let res = document.getElementById('resImc');
 
 
-form.addEventListener('submit',function(event){    
-   
-    var altura = parseFloat(document.getElementById('altura').value) / 100;
-    var peso = parseFloat(document.getElementById('peso').value);    
-    var imc = peso / (altura * altura).toFixed(2);    
-    res.innerHTML = (`O valor do IMC para ${nome.value} é ${imc.toFixed(2)}`);
-  
-    
+
+form.addEventListener('submit', function(event){
+    let altura = parseFloat(document.getElementById('altura').value) / 100;
+    let peso = parseFloat(document.getElementById('peso').value);
+    let quadrado = (altura * altura);
+    let imc = peso / quadrado.toFixed(2);
+    let tabela = ('');
+    if(imc < 18.50){
+        tabela = ('<span style="color: #f00";>Atenção<br>Abaixo do peso</span>');
+    }else if(imc >= 18.50 && imc <= 24.99 ){
+        tabela = ('Parabéns<br>Peso normal');
+    }else if(imc >= 25.00 && imc <=29.99){
+        tabela = ('<span style="color: #f00";>Atenção<br>Sobrepeso</span>');
+    }else if(imc >= 30.00 && imc < 34.99 ){
+        tabela = ('<span style="color: #f00";>Atenção<br>Obesidade Grau I</span>');
+    }else if(imc >= 35.00 && imc <= 39.99 ){
+        tabela = ('<span style="color: #f00";>Atenção<br>Obesidade Grau II</span>');
+    }else{
+        tabela = ('<span style="color: #f00";>Atenção<br>Obesidade Grau III</span>');
+    }
+
+    res.innerHTML = (`${nome.value} o valor do seu IMC é ${imc.toFixed(2)}<br>${tabela}`);
     event.preventDefault();
-    
 });
 
-
+form.addEventListener('reset', function(event){
+    location.reload();
+    event.preventDefault();
+});
 
 
 
